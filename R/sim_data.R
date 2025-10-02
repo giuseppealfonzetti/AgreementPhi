aabd6593b6b4ffc2c41efec80c8bc64acac20ec3
Giuseppe Alfonzetti <32683739+giuseppealfonzetti@users.noreply.github.com>
2025-10-02 23:20:48 +0200

    update sim_data with two-way support

--- R/sim_data.R
+++ R/sim_data.R
@@ -1,28 +1,34 @@
 #' Simulate ordinal or continuous (0,1) ratings
 #'
 #' @param J Number of items.
-#' @param B Budget per item.
+#' @param B Budget per item (i.e. number of workers assigned to each item).
+#' @param W Maximum number of workers.
 #' @param AGREEMENT General agreement.
 #' @param ALPHA Item-specific intercepts.
 #' @param DATA_TYPE Choose between `ordinal` or `continuous`.
 #' @param K Number of categories in case of ordinal data.
 #' @param SEED RNG seed.
 #'
-#' @return Returns a dataframe with columns id_items, id_items_obs, rating
+#' @return Returns a dataframe with columns id_items, id_worker and rating
 #' @export
 sim_data <- function(
   J,
   B,
+  W = J * 2,
   AGREEMENT,
   ALPHA,
+  BETA = NULL,
   DATA_TYPE = c("ordinal", "continuous"),
-  K = 5,
+  K = 6,
   SEED = 123
 ) {
+  set.seed(SEED)
   stopifnot(J > 0)
   stopifnot(AGREEMENT >= 0)
   stopifnot(AGREEMENT <= 1)
   stopifnot(K > 1)
+  stopifnot(W > 1)
+  stopifnot(B <= W)
   stopifnot(is.numeric(ALPHA))
   stopifnot(length(ALPHA) == J)
 
@@ -32,11 +38,15 @@ sim_data <- function(
   n_obs <- J * B
   precision <- agr2prec(AGREEMENT)
   obs_item_ind <- rep(1:J, each = B)
-  obs_mu <- plogis(ALPHA[obs_item_ind])
+  obs_worker_ind <- as.vector(replicate(J, sample(1:W, B, replace = FALSE)))
+  if (is.null(BETA)) {
+    BETA <- rep(0, W)
+  }
+  obs_mu <- plogis(ALPHA[obs_item_ind] + BETA[obs_worker_ind])
+
   obs_a <- obs_mu * precision
   obs_b <- (1 - obs_mu) * precision
 
-  set.seed(SEED)
   obs_beta <- apply(cbind(obs_a, obs_b), 1, function(par) {
     rbeta(1, par[1], par[2])
   })
@@ -44,14 +54,19 @@ sim_data <- function(
   if (DATA_TYPE == "ordinal") {
     obs_y <- cont2ord(obs_beta, K)
   } else {
-    obs_y <- obs_beta
+    obs_y <- ifelse(
+      abs(obs_beta - 1) < .Machine$double.eps / 2,
+      obs_beta - 1e-5,
+      obs_beta
+    )
+    obs_y <- ifelse(obs_y < .Machine$double.eps / 2, 1e-5, obs_y)
   }
 
-  dt <- data.frame(cbind(
+  dt <- data.frame(
     id_item = obs_item_ind,
-    id_item_obs = obs_item_ind,
+    id_worker = obs_worker_ind,
     rating = obs_y * 1.0
-  ))
+  )
 
   return(dt)
 }
--- tests/testthat/test-sim_data.R
+++ tests/testthat/test-sim_data.R
@@ -1,7 +1,7 @@
+#### one-way ####
 items <- 100
 budget_per_item <- 10
-n_obs <- items * budget_per_item
-k <- 5
+k <- 10
 alphas <- rnorm(items)
 agr <- runif(1)
 
@@ -14,3 +14,30 @@ dt <- sim_data(
   K = k,
   SEED = 123
 )
+head(dt)
+length(unique(dt$id_worker))
+
+#### two-way ####
+items <- 100
+budget_per_item <- 10
+workers <- 300
+k <- 10
+alphas <- rnorm(items)
+betas <- rnorm(workers)
+agr <- runif(1)
+dt2 <- sim_data(
+  J = items,
+  B = budget_per_item,
+  W = workers,
+  AGREEMENT = .1,
+  ALPHA = alphas,
+  # BETA = betas,
+  DATA_TYPE = "continuous",
+  K = k,
+  SEED = 123
+)
+
+head(dt2)
+length(unique(dt2$id_worker))
+as.vector(replicate(items, sample(1:workers, budget_per_item, replace = FALSE)))
+max(table(dt2$id_worker, dt2$id_item))
