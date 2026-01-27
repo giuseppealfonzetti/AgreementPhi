c3f98a686cebfbdd230ae9d4ef8be1ee48e48528
Giuseppe Alfonzetti <32683739+giuseppealfonzetti@users.noreply.github.com>
2026-01-27 09:59:59 +0100

    add example to plot rll

--- R/plot.R
+++ R/plot.R
@@ -7,7 +7,55 @@
 #' @param P_SE standard error for agreement estimate from profile likelihood
 #' @param CONFIDENCE Confidence level to construct confidence intervals
 #'
+#' @examples
+#' set.seed(321)
 #'
+#' # setting dimension
+#' items <- 50
+#' budget_per_item <- 5
+#' n_obs <- items * budget_per_item
+#' workers <- 50
+#'
+#' # item-specific intercepts to generate the data
+#' alphas <- runif(items, -2, 2)
+#'
+#' # true agreement (between 0 and 1)
+#' agr <- .6
+#'
+#' # generate continuous rating in (0,1)
+#' dt_oneway <- sim_data(
+#'   J = items,
+#'   B = budget_per_item,
+#'   AGREEMENT = agr,
+#'   ALPHA = alphas,
+#'   DATA_TYPE = "continuous",
+#'   SEED = 123
+#' )
+#'
+#' # estimation via oneway specification
+#' fit <- agreement(
+#'   RATINGS = dt_oneway$rating,
+#'   ITEM_INDS = dt_oneway$id_item,
+#'   WORKER_INDS = dt_oneway$id_worker,
+#'   METHOD = "modified",
+#'   NUISANCE = c("items"),
+#'   VERBOSE = TRUE
+#' )
+#' # get standard error and confidence interval
+#' ci <- get_ci(fit)
+#' ci
+#'
+#' # compute log-likelihood over a grid
+#' range_ll <- get_range_ll(fit)
+#'
+#' # utility plot function for relative log-likelihood
+#' plot_rll(
+#'   D = range_ll,
+#'   M_EST = fit$modified$agreement,
+#'   P_EST = fit$profile$agreement,
+#'   M_SE = ci$agreement_se,
+#'   CONFIDENCE=.95
+#' )
 #' @export
 #' @importFrom graphics abline legend lines plot rect axis par
 #' @importFrom grDevices adjustcolor
