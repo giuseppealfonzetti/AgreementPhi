049caa2e552f50ae907f3579a5f6a74a751c540c
Giuseppe Alfonzetti <32683739+giuseppealfonzetti@users.noreply.github.com>
2026-05-29 14:57:10 +0200

    expose downloading function for roitero's data

--- data-raw/roitero2021.R
+++
@@ -1,7 +0,0 @@
-dat <- read.csv("data-raw/dataframe.csv", header = TRUE)
-pbapply::pboptions(type = "txt")
-roitero2021 <- subset(dat, relevance_scale == "S100")[, setdiff(
-  colnames(dat),
-  c("normalized_relevance_score", "relevance_scale")
-)]
-usethis::use_data(roitero2021, overwrite = TRUE)
